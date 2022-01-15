c3f3f7c8e150f83a352906c7310a48f6f240a72e
kasprzyk <piotkas1@gmail.com>
2022-01-15 12:21:35 +0100

    showing env

--- task6/app.py/app.py
+++ task6/app.py/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
+import os
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, App from docker'
+    return "Test strony we Flask - " + os.environ["INFOSHARE"]
